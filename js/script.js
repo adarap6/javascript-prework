58fc8f4a16fe98dcd1531b22fabca77534421595
Ivan Parada <ivan.parada.it@gmail.com>
2019-08-09 10:28:08 +0200

    add script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
-let computerMove = `kamień`;
-let playerMove = `papier`
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else(randomNumber == 2){
+    computerMove = 'papier'
+} else(randomNumber == 3)
+{computerMove = 'nozyce'}
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch'
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else(playerInput == '2'){
+    playerMove = 'papier'
+} else(playerInput == '3'){
+    playerMove = 'nożyce'
+}
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  } else(computerMove == 'kameń' && playerMove == 'nożyce'){
+      printMessage('Computer wygral')
+  } else(computerMove == 'papier' && playerMove == 'kamień'){
+        printMessage('Computer wygral')
+  } else(computerMove == 'papier' && playerMove == 'nożyce'){
+        printMessage('Ty wygral')
+  } else(computerMove == 'nożyce' && playerMove == 'kameń'){
+      printMessage('Ty wygral')
+  } else(computerMove == 'nożyce' && playerMove == 'papier'){
+      printMessage('Computer wygral')
+  } else(computerMove == 'kamień' && playerMove == 'kamień'){
+      printMessage(Remis)
+  } else(computerMove =='papier' && playerMove == 'papier'){
+      printMessage(Remis)
+  } else(computerMove == 'nożyce' && playerMove == 'nożyce'){
+      printMessage(Remis)
+  } else(playerMove == playerMove){
+      printMessage('Nieznany ruch')
+  } else(computerMove == computerMove){
+        printMessage('Nieznany ruch')
+  }
